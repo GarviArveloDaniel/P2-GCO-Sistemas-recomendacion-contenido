d183b19d63411f5fc4ded9513ad090df741998df
Guille <alu0101505688@ull.edu.es>
2024-10-30 11:43:40 +0000

    quitando signos de puntuación

--- src/procesar_documentos.py
+++ src/procesar_documentos.py
@@ -1,10 +1,14 @@
 import json
+import re
 from collections import Counter
 
 def procesar_fichero(fichero_principal, fichero_eliminar, fichero_lemas):
     # Leer el fichero principal y almacenar las palabras en una lista junto con sus índices originales
     with open(fichero_principal, 'r', encoding='utf-8') as f:
-        palabras = f.read().split()
+        texto = f.read()
+        texto = re.sub(r'[^\w\s]', '', texto)
+        palabras = texto.split()
+
     palabras_con_indices = [(indice, palabra) for indice, palabra in enumerate(palabras)]
     
     # Leer el fichero de palabras a eliminar y almacenarlas en un conjunto
