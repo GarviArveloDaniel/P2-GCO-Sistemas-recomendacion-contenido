ade4cd8489dcf8e9db2f33ab99fa19bb43a75d3a
Guille <alu0101505688@ull.edu.es>
2024-10-30 11:13:14 +0000

    Añadidos indices

--- src/procesar_documentos.py
+++ src/procesar_documentos.py
@@ -1,23 +1,24 @@
 import json
 
 def procesar_fichero(fichero_principal, fichero_eliminar, fichero_lemas):
-    # Leer el fichero principal y almacenar las palabras en una lista
+    # Leer el fichero principal y almacenar las palabras en una lista junto con sus índices originales
     with open(fichero_principal, 'r', encoding='utf-8') as f:
         palabras = f.read().split()
+    palabras_con_indices = [(indice, palabra) for indice, palabra in enumerate(palabras)]
     
     # Leer el fichero de palabras a eliminar y almacenarlas en un conjunto
     with open(fichero_eliminar, 'r', encoding='utf-8') as f:
         palabras_a_eliminar = set(f.read().split())
     
-    # Filtrar la lista de palabras para eliminar las que están en el conjunto de palabras a eliminar
-    palabras_filtradas = [palabra for palabra in palabras if palabra not in palabras_a_eliminar]
+    # Filtrar la lista de palabras con índices para eliminar las que están en el conjunto de palabras a eliminar
+    palabras_filtradas = [(indice, palabra) for indice, palabra in palabras_con_indices if palabra not in palabras_a_eliminar]
     
     # Leer el fichero de lematización en formato JSON y cargarlo como diccionario
     with open(fichero_lemas, 'r', encoding='utf-8') as f:
         lemas = json.load(f)
     
     # Reemplazar las palabras filtradas con su lema si existe en el diccionario
-    palabras_finales = [lemas.get(palabra, palabra) for palabra in palabras_filtradas]
+    palabras_finales = [(indice, lemas.get(palabra, palabra)) for indice, palabra in palabras_filtradas]
     
     return palabras_finales
 
